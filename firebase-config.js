66049dd4c4237273478ff2826c9548678cb228ed
egmCeiRyu <egami@ceiryu.com>
2025-05-20 11:11:14 +0900

    subm

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// firebase-config.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyAD69rtBwPZfzTGi_TDvky6wnflw9bBIxg",
+  authDomain: "clicksample-ec2c6.firebaseapp.com",
+  projectId: "clicksample-ec2c6",
+  storageBucket: "clicksample-ec2c6.appspot.com",
+  messagingSenderId: "603343923123",
+  appId: "1:603343923123:web:eef81eaeb5dd367911cdc6",
+  measurementId: "G-26TE0NM69V"
+};
